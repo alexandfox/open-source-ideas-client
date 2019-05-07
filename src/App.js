2c07ed8050f74b075fbaf57a75473bcd552b9fe5
Mimi <mi.amsalem@gmail.com>
2019-05-07 14:23:34 +0200

    Structured the file + adding form for create idea

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from "react-router-dom";
+import NavMain from "./components/NavMain";
+import Home from "./pages/Home"
+import CreateIdea from "./pages/CreateIdea"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>hello</h1> */}
+      <NavMain />
+      <main>
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/create-idea" component={CreateIdea} exact />
+        </Switch>
+      </main>
     </div>
+
   );
 }
 
