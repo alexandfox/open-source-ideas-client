abc9ff1efacdce840515597ca48225541f415c61
Mimi <mi.amsalem@gmail.com>
2019-05-09 12:29:32 +0200

    tags works

---
+++ src/dataCategories.js
@@ -0,0 +1,21 @@
+module.exports =
+  {
+
+    "categories": [
+      "Music",
+      "Sport",
+      "Books",
+      "Environment",
+      "Fashion",
+      "Design",
+      "Art & Photo",
+      "Food",
+      "Games",
+      "Tech",
+      "Craft",
+      "Movies & Video",
+      "Health",
+      "Education"
+    ]
+
+  };
