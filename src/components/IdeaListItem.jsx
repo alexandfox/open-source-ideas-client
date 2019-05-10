a11f1ec840e75b05257c919551997f948ae3c53d
alex fox <to.alexfox@gmail.com>
2019-05-10 15:46:07 +0200

    save route working fully

--- src/components/IdeaListItem.jsx
+++ src/components/IdeaListItem.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {Component} from "react"
 import DraftEdit from "./DraftEdit"
 import UpvoteDownvote from "../components/UpvoteDownvote";
 
@@ -8,28 +8,44 @@ import UpvoteDownvote from "../components/UpvoteDownvote";
 // isMine      --> Boolean; if true && !props.isPublic --> show draft/edit
             // else --> show upvote/downvote
 
-function ideaItem(props) {
-  console.log("idea list item props ", props)
-  return (
-    <div className="ideaItem">
-      <a className="listTitleLink" href={`/idea/${props._id}`}>{props.title}</a>
-      <div className="listIdeaDescription">{props.description}</div>
-      <p className="ideaCreator">Creator: <a href={`/@${props.creator && props.creator.name}`}>{props.creator && props.creator.name}</a></p>
+// .isPublic is boolean true === "published", false === "draft"
+// if status === false:   --> displays DraftEdit component
+// if status === true:    --> displays Upvote component
 
-      {/* {!props.isMine && <UpvoteDownvote ideaId={props._id} loggedUser={props.loggedUser} />} */}
+class ideaItem extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      // isDraft : props.isPublic
+    }
+    console.log("idea list item props ", props)
+  }
+  
+  render() {
+    return (
+      <div className="ideaItem">
+        {/* MY DRAFTS */}
+        {(this.props.isMine && !this.props.isPublic) ? 
+        <div className="draftItem">
+          <a className="listDraftLink" href={`/idea/${this.props._id}`}>{this.props.title}</a>
+          <DraftEdit id={this.props._id} /> 
+        </div>
+        : 
+        <UpvoteDownvote ideaId={this.props._id} loggedUser={this.props.loggedUser} />}
 
-      {(props.isMine && !props.isPublic) ? <DraftEdit id={props._id} /> : <UpvoteDownvote ideaId={props._id} loggedUser={props.loggedUser} />}
+        <a className="listTitleLink" href={`/idea/${this.props._id}`}>{this.props.title}</a>
+        <div className="listIdeaDescription">{this.props.description}</div>
+        <p className="ideaCreator">Creator: <a href={`/@${this.props.creator && this.props.creator.name}`}>{this.props.creator && this.props.creator.name}</a></p>
 
-      {/* add check for props.isPublic */}
-      {/* <DraftEdit id={props._id} /> */}
-    </div>
-  )
+        {/* {!this.props.isMine && <UpvoteDownvote ideaId={this.props._id} loggedUser={this.props.loggedUser} />} */}
+
+        
+
+        {/* add check for this.props.isPublic */}
+        {/* <DraftEdit id={this.props._id} /> */}
+      </div>
+    )
+  }
 }
 
 export default ideaItem
-
-// Props is an idea OBJECT
-
-// .isPublished is boolean true === "published", false === "draft"
-// if status === false:   --> displays DraftEdit component
-// if status === true:    --> displays Upvote component
--- src/components/form-idea/FormCreateIdea.jsx
+++ src/components/form-idea/FormCreateIdea.jsx
@@ -89,9 +89,9 @@ class FormCreateIdea extends Component {
   handleSubmit = (evt) => {
     evt.preventDefault();
     const { title, description, category, tags } = this.state;
-    if (!title || !description || !category || !tags.length) {
-      return console.log("nope")
-    }
+    // if (!title || !description || !category || !tags.length) {
+    //   return console.log("nope")
+    // }
 
     createOneIdea({...this.state, isPublic: true})
       .then(res => {
