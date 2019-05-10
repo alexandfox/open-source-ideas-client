d32ba9429155aff26d205c454b4b6786c8d85fde
alex fox <to.alexfox@gmail.com>
2019-05-10 16:12:37 +0200

    merging to get changes

--- src/api/apiHandler.js
+++ src/api/apiHandler.js
@@ -9,7 +9,7 @@ export const getUserByName = name => axios.get(`${APIURL}/user/name/${name}`);
 
 export const updateOneUser = (id, infos) => axios.put(`${APIURL}/user/${id}`, infos);
 
-//DEAS METHODS-----------------------------------------------------------------
+//IDEAS METHODS-----------------------------------------------------------------
 
 export const getAllIdeas = (obj) => axios.get(`${APIURL}/idea`, obj);
 
