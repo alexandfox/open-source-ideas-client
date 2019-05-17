74e4c9be59c01ecb8a7264d74ea94c39a5f7da2c
Mimi <mi.amsalem@gmail.com>
2019-05-17 14:21:58 +0200

    change some css on modal login

--- src/components/IdeaListItem.jsx
+++ src/components/IdeaListItem.jsx
@@ -65,7 +65,7 @@ class ideaItem extends Component {
                 <Moment className="publicItemDate" fromNow>{this.props.created_at}</Moment>
               </div>
             </div>
-            {!this.props.creator._id && <SharedOptions id={this.props._id} sendToParent={(e) => this.sendToParent(e)}/>}
+            {/* {!this.props.creator._id && <SharedOptions id={this.props._id} sendToParent={(e) => this.sendToParent(e)}/>} */}
           </div>
 
           <div className="publicItemUpvote">
